1c6974b565554d0a23753487b40da6d9d9365f39
Stefan Vet <125643909+StefanV3T@users.noreply.github.com>
2023-10-13 12:09:42 +0200

    Weerbericht

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json; 
+
+var client = new HttpClient();
+client.BaseAddress = new Uri($"https://api.open-meteo.com/v1/");
+
+var response = client.GetAsync($"forecast?latitude=52.52&longitude=13.41&current=temperature_2m,relativehumidity_2m,apparent_temperature,is_day,rain&timeformat=unixtime&timezone=Europe%2FBerlin").Result;
+string json = response.Content.ReadAsStringAsync().Result;
+
+var weather = JsonSerializer.Deserialize<Weerbericht>(json);
+var current = weather.current;
+
+    
+
+
+Console.WriteLine($"Temperature: {current.temperature_2m}*c \r\nFeels like: {current.apparent_temperature}*c \r\nHumidity: {current.relativehumidity_2m}% \r\nRainfall: {current.rain}mm");
---
+++ Weerbericht.cs
@@ -0,0 +1,14 @@
+﻿public class WeerberichtContent
+{
+    public decimal temperature_2m { get; set; }
+    public decimal apparent_temperature {  get; set; }
+    public decimal relativehumidity_2m { get; set; }
+    public decimal rain {  get; set; }
+
+}
+
+public class Weerbericht
+{ 
+    public WeerberichtContent current {  get; set; }
+}
+
